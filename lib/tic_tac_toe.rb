15c9e227cea6c8c483fcf25ef11b3aa03d0d0287
Amy Tu <siamesebb@gmail.com>
2018-05-23 16:28:39 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,9 +17,9 @@ class TicTacToe
   
   def display_board
     puts " #{board[0]}" | " #{board[1]}" | "#{board[2]}"
+    puts "-----------"
+    puts
     puts 
-    puts
-    puts
     puts
     
 end
