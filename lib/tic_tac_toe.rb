1c1d6fae8c1d7a2cf8cf6ad460d82b88338c0f19
Amy Tu <siamesebb@gmail.com>
2018-05-23 16:43:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ class TicTacToe
   end
   
   def position_taken?(position)
-    if @board[position] == "X" ||@board[position] == "O"
+    if @board[position] == "X" || @board[position] == "O"
       true 
     else
       false 
