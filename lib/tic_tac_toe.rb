b8302768a563ec15a69115e9c31181d462131b13
Amy Tu <siamesebb@gmail.com>
2018-05-23 19:59:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,7 @@ class TicTacToe
   end
   
   def winner
-    WIN_COMBINATIONS.each do |combo|
+    WIN_COMBINATIONS.detect do |combo|
       if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" 
         return "X"
       elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
